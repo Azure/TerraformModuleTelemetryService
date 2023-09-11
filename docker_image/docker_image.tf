1c4594408ba96a6058d524f6840855f03ab57739
lonegunmanb <hezijie@microsoft.com>
2023-09-11 07:01:18 +0000

    add trigger to redeploy docker image when telemetry proxy code has been changed

--- docker_image/docker_image.tf
+++ docker_image/docker_image.tf
@@ -1,3 +1,9 @@
+resource "null_resource" "go_code_keeper" {
+  triggers = {
+    code_hash = filemd5("${path.module}/telemetry/main.go")
+  }
+}
+
 resource "docker_image" "proxy" {
   name = "${var.registry_url}/telemetry_proxy"
   build {
@@ -7,9 +13,17 @@ resource "docker_image" "proxy" {
   triggers = {
     code_hash = filemd5("${path.module}/telemetry/main.go")
   }
+
+  lifecycle {
+    replace_triggered_by = [null_resource.go_code_keeper]
+  }
 }
 
 resource "docker_registry_image" "proxy" {
   name = docker_image.proxy.name
   keep_remotely = true
+
+  lifecycle {
+    replace_triggered_by = [null_resource.go_code_keeper]
+  }
 }
