72817a9aa381417cc3c3439a6ecb4de91295aa84
lonegunmanb <hezijie@microsoft.com>
2023-09-12 07:44:50 +0000

    fix all remaining bugs, install ca-certificates on mariner container because the base image seems doesn't contain ms's root cert any more

--- main.tf
+++ main.tf
@@ -17,19 +17,20 @@ module "docker_image" {
 }
 
 module "container_apps" {
-  source              = "./container_apps"
-  acr_url             = module.acr.registry_url
-  acr_user_name       = module.acr.pull_username
-  acr_user_password   = module.acr.pull_password
-  docker_image        = module.docker_image.docker_image
-  location            = module.resource_group.resource_group_location
-  resource_group_name = module.resource_group.resource_group_name
-  subnet_id           = module.acr.container_apps_subnet_id
+  source               = "./container_apps"
+  acr_url              = module.acr.registry_url
+  acr_user_name        = module.acr.pull_username
+  acr_user_password    = module.acr.pull_password
+  docker_image         = module.docker_image.docker_image
+  location             = module.resource_group.resource_group_location
+  resource_group_name  = module.resource_group.resource_group_name
+  subnet_id            = module.acr.container_apps_subnet_id
+  telemetry_proxy_diag = true
 }
 
 module "endpoint_blob" {
   source                  = "./blob_for_url"
-  endpoint                = module.container_apps.app_url["telemetry_proxy"]
+  endpoint                = "${module.container_apps.app_url["telemetry_proxy"]}/telemetry"
   resource_group_location = module.resource_group.resource_group_location
   resource_group_name     = module.resource_group.resource_group_name
 }
