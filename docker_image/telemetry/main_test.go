2629a0db9647e75828d5904958fd923157877eb8
zjhe <lonegunmanb@hotmail.com>
2024-10-15 11:17:12 +0800

    add module source allow list

--- docker_image/telemetry/main_test.go
+++ docker_image/telemetry/main_test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/iris-contrib/httpexpect/v2"
-	"github.com/kataras/iris/v12"
-	"github.com/stretchr/testify/assert"
 	"testing"
 
+	"github.com/iris-contrib/httpexpect/v2"
+	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/httptest"
 	"github.com/microsoft/ApplicationInsights-Go/appinsights"
+	"github.com/stretchr/testify/assert"
 )
 
 var _ telemetryClient = &mockClient{}
@@ -32,8 +32,9 @@ func Test_TelemetryGetShouldReturnOk(t *testing.T) {
 func Test_TelemetryWithoutEventShouldReturnError(t *testing.T) {
 	e, _ := sut(t)
 	body := map[string]string{
-		"foo":         "bar",
-		"resource_id": "dummyId",
+		"foo":           "bar",
+		"resource_id":   "dummyId",
+		"module_source": "registry.terraform.io/Azure/foo",
 	}
 	e.POST(endpoint).WithJSON(body).Expect().Status(iris.StatusBadRequest)
 }
@@ -41,8 +42,9 @@ func Test_TelemetryWithoutEventShouldReturnError(t *testing.T) {
 func Test_TelemetryWithoutResourceIdShouldReturnError(t *testing.T) {
 	e, _ := sut(t)
 	body := map[string]string{
-		"event": "create",
-		"foo":   "bar",
+		"event":         "create",
+		"foo":           "bar",
+		"module_source": "registry.terraform.io/Azure/foo",
 	}
 	e.POST(endpoint).WithJSON(body).Expect().Status(iris.StatusBadRequest)
 }
@@ -50,9 +52,10 @@ func Test_TelemetryWithoutResourceIdShouldReturnError(t *testing.T) {
 func Test_TelemetryShouldBeSendToAppInsights(t *testing.T) {
 	e, c := sut(t)
 	body := map[string]string{
-		"event":       "create",
-		"resource_id": "resourceId",
-		"foo":         "bar",
+		"event":         "create",
+		"resource_id":   "resourceId",
+		"foo":           "bar",
+		"module_source": "registry.terraform.io/Azure/foo",
 	}
 	e.POST(endpoint).WithJSON(body).Expect().Status(iris.StatusOK)
 	assert.Equal(t, 1, len(c.telemetries))
@@ -63,8 +66,37 @@ func Test_TelemetryShouldBeSendToAppInsights(t *testing.T) {
 }
 
 func sut(t *testing.T) (*httpexpect.Expect, *mockClient) {
+	t.Setenv("SOURCE_REGEX_0", "registry.terraform.io/[A|a]zure/.+")
+	t.Setenv("SOURCE_REGEX_1", "registry.opentofu.io/[A|a]zure/.+")
+	t.Setenv("SOURCE_REGEX_2", "git::https://github\\.com/[A|a]zure/.+")
+	t.Setenv("SOURCE_REGEX_3", "git::ssh:://git@github\\.com/[A|a]zure/.+")
 	client := &mockClient{}
 	app := newApp(client)
 	e := httptest.New(t, app)
 	return e, client
+}
+
+func Test_ShouldRetrieveFilteredSourceListFromMultipleEnvs(t *testing.T) {
+	t.Setenv("SOURCE_REGEX_0", "source0")
+	t.Setenv("SOURCE_REGEX_1", "source1")
+
+	list := filterSources()
+	assert.Len(t, list, 2)
+}
+
+func Test_EmptySourceList(t *testing.T) {
+	list := filterSources()
+	assert.Empty(t, list)
+}
+
+func Test_UnauthorizedSourceTelemetryShouldBeReject(t *testing.T) {
+	e, c := sut(t)
+	body := map[string]string{
+		"event":         "create",
+		"resource_id":   "resourceId",
+		"foo":           "bar",
+		"module_source": "registry.terraform.io/foo/bar",
+	}
+	e.POST(endpoint).WithJSON(body).Expect().Status(iris.StatusForbidden)
+	assert.Empty(t, c.telemetries)
 }
