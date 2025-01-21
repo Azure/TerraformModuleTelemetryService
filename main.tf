2af45bb79bf53cd0011e7dfb1824b328b18bec95
hezijie <lonegunmanb@hotmail.com>
2025-01-21 09:49:26 +0800

    fix config

--- main.tf
+++ main.tf
@@ -16,15 +16,16 @@ module "docker_image" {
 }
 
 module "container_apps" {
-  source               = "./container_apps"
-  acr_url              = module.acr.registry_url
-  acr_user_name        = module.acr.pull_username
-  acr_user_password    = module.acr.pull_password
-  docker_image         = module.docker_image.docker_image
-  location             = module.resource_group.resource_group_location
-  resource_group_name  = module.resource_group.resource_group_name
-  subnet_id            = module.acr.container_apps_subnet_id
-  telemetry_proxy_diag = true
+  source                                   = "./container_apps"
+  acr_url                                  = module.acr.registry_url
+  acr_user_name                            = module.acr.pull_username
+  acr_user_password                        = module.acr.pull_password
+  application_insights_instrumentation_key = var.application_insights_instrumentation_key
+  docker_image                             = module.docker_image.docker_image
+  location                                 = module.resource_group.resource_group_location
+  resource_group_name                      = module.resource_group.resource_group_name
+  subnet_id                                = module.acr.container_apps_subnet_id
+  telemetry_proxy_diag                     = true
 }
 
 module "endpoint_blob" {
@@ -32,18 +33,4 @@ module "endpoint_blob" {
   endpoint                = "${module.container_apps.app_url["telemetry_proxy"]}/telemetry"
   resource_group_location = module.resource_group.resource_group_location
   resource_group_name     = module.resource_group.resource_group_name
-}
-
-data "azuread_group" "modtm_reader" {
-  display_name = "Modtm Telemetry Reader"
-}
-
-resource "azurerm_role_assignment" "telemetry_reader" {
-  for_each = tomap({
-    application_insight = module.container_apps.application_insights_id
-    log_analytics       = module.container_apps.log_analytics_workspace
-  })
-  role_definition_name = "Reader"
-  scope                = sensitive(each.value)
-  principal_id         = sensitive(data.azuread_group.modtm_reader.object_id)
 }
