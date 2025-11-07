9ceb052913097db3ac201e379c813551341e8307
lonegunmanb <hezijie@microsoft.com>
2025-11-07 03:21:37 +0000

    update backend storage account name, prepare test run for migration

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "tfmod1espoolstatestorage"
-    resource_group_name  = "bambrane-runner-state"
+    storage_account_name = "avmtelemetrystate"
+    resource_group_name  = "avm-telemetry-state"
     container_name       = "azure-verified-tfmod-runner-state"
     key                  = "telemetry/terraform.tfstate"
     snapshot             = true
